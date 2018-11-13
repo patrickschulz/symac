bafa0015ae380b073737e66d260d6c83197b6c8f
Patrick Schulz <pschulz@posteo.de>
2018-11-13 11:46:30 +0100

    Added spirit calculator with evaluation.

--- src/calculator/main.cpp
+++ src/calculator/main.cpp
@@ -1,5 +1,4 @@
 /*=============================================================================
-    Copyright (c) 2001-2011 Hartmut Kaiser
     Copyright (c) 2001-2011 Joel de Guzman
 
     Distributed under the Boost Software License, Version 1.0. (See accompanying
@@ -7,128 +6,243 @@
 =============================================================================*/
 ///////////////////////////////////////////////////////////////////////////////
 //
-//  Plain calculator example demonstrating the grammar. The parser is a
-//  syntax checker only and does not do any semantic evaluation.
+//  A Calculator example demonstrating generation of AST. The AST,
+//  once created, is traversed, 1) To print its contents and
+//  2) To evaluate the result.
 //
-//  [ JDG May 10, 2002 ]        spirit1
-//  [ JDG March 4, 2007 ]       spirit2
-//  [ HK November 30, 2010 ]    spirit2/utree
+//  [ JDG April 28, 2008 ]      For BoostCon 2008
+//  [ JDG February 18, 2011 ]   Pure attributes. No semantic actions.
 //
 ///////////////////////////////////////////////////////////////////////////////
 
-// #define BOOST_SPIRIT_DEBUG
+// Spirit v2.5 allows you to suppress automatic generation
+// of predefined terminals to speed up complation. With
+// BOOST_SPIRIT_NO_PREDEFINED_TERMINALS defined, you are
+// responsible in creating instances of the terminals that
+// you need (e.g. see qi::uint_type uint_ below).
+#define BOOST_SPIRIT_NO_PREDEFINED_TERMINALS
+
+#if defined(_MSC_VER)
+# pragma warning(disable: 4345)
+#endif
 
 #include <boost/config/warning_disable.hpp>
-#include <boost/spirit/include/support_utree.hpp>
 #include <boost/spirit/include/qi.hpp>
-#include <boost/spirit/include/phoenix_operator.hpp>
-#include <boost/spirit/include/phoenix_function.hpp>
+#include <boost/variant/recursive_variant.hpp>
+#include <boost/variant/apply_visitor.hpp>
+#include <boost/fusion/include/adapt_struct.hpp>
+#include <boost/foreach.hpp>
 
 #include <iostream>
 #include <string>
+
+namespace client { namespace ast
+{
+    ///////////////////////////////////////////////////////////////////////////
+    //  The AST
+    ///////////////////////////////////////////////////////////////////////////
+    struct nil {};
+    struct signed_;
+    struct program;
+
+    typedef boost::variant<
+            nil
+          , double
+          , boost::recursive_wrapper<signed_>
+          , boost::recursive_wrapper<program>
+        >
+    operand;
+
+    struct signed_
+    {
+        char sign;
+        operand operand_;
+    };
+
+    struct operation
+    {
+        char operator_;
+        operand operand_;
+    };
+
+    struct program
+    {
+        operand first;
+        std::list<operation> rest;
+    };
+}}
+
+BOOST_FUSION_ADAPT_STRUCT(
+    client::ast::signed_,
+    (char, sign)
+    (client::ast::operand, operand_)
+)
+
+BOOST_FUSION_ADAPT_STRUCT(
+    client::ast::operation,
+    (char, operator_)
+    (client::ast::operand, operand_)
+)
+
+BOOST_FUSION_ADAPT_STRUCT(
+    client::ast::program,
+    (client::ast::operand, first)
+    (std::list<client::ast::operation>, rest)
+)
+
+namespace client { namespace ast
+{
+    ///////////////////////////////////////////////////////////////////////////
+    //  The AST Printer
+    ///////////////////////////////////////////////////////////////////////////
+    struct printer
+    {
+        typedef void result_type;
+
+        void operator()(nil) const {}
+        void operator()(double n) const { std::cout << n; }
+
+        void operator()(operation const& x) const
+        {
+            boost::apply_visitor(*this, x.operand_);
+            switch (x.operator_)
+            {
+                case '+': std::cout << " add"; break;
+                case '-': std::cout << " subt"; break;
+                case '*': std::cout << " mult"; break;
+                case '/': std::cout << " div"; break;
+            }
+        }
+
+        void operator()(signed_ const& x) const
+        {
+            boost::apply_visitor(*this, x.operand_);
+            switch (x.sign)
+            {
+                case '-': std::cout << " neg"; break;
+                case '+': std::cout << " pos"; break;
+            }
+        }
+
+        void operator()(program const& x) const
+        {
+            boost::apply_visitor(*this, x.first);
+            BOOST_FOREACH(operation const& oper, x.rest)
+            {
+                std::cout << ' ';
+                (*this)(oper);
+            }
+        }
+    };
+
+    ///////////////////////////////////////////////////////////////////////////
+    //  The AST evaluator
+    ///////////////////////////////////////////////////////////////////////////
+    struct eval
+    {
+        typedef double result_type;
+
+        double operator()(nil) const { BOOST_ASSERT(0); return 0; }
+        double operator()(double n) const { return n; }
+
+        double operator()(operation const& x, double lhs) const
+        {
+            double rhs = boost::apply_visitor(*this, x.operand_);
+            switch (x.operator_)
+            {
+                case '+': return lhs + rhs;
+                case '-': return lhs - rhs;
+                case '*': return lhs * rhs;
+                case '/': return lhs / rhs;
+            }
+            BOOST_ASSERT(0);
+            return 0;
+        }
+
+        double operator()(signed_ const& x) const
+        {
+            double rhs = boost::apply_visitor(*this, x.operand_);
+            switch (x.sign)
+            {
+                case '-': return -rhs;
+                case '+': return +rhs;
+            }
+            BOOST_ASSERT(0);
+            return 0;
+        }
+
+        double operator()(program const& x) const
+        {
+            double state = boost::apply_visitor(*this, x.first);
+            BOOST_FOREACH(operation const& oper, x.rest)
+            {
+                state = (*this)(oper, state);
+            }
+            return state;
+        }
+    };
+}}
 
 namespace client
 {
     namespace qi = boost::spirit::qi;
     namespace ascii = boost::spirit::ascii;
-    namespace spirit = boost::spirit;
-
-    struct expr
-    {
-        template <typename T1, typename T2 = void>
-        struct result { typedef void type; };
-
-        expr(char op) : op(op) {}
-
-        void operator()(spirit::utree& expr, spirit::utree const& rhs) const
-        {
-            spirit::utree lhs;
-            lhs.swap(expr);
-            expr.push_back(spirit::utf8_symbol_range_type(&op, &op+1));
-            expr.push_back(lhs);
-            expr.push_back(rhs);
-        }
-
-        char const op;
-    };
-    boost::phoenix::function<expr> const plus = expr('+');
-    boost::phoenix::function<expr> const minus = expr('-');
-    boost::phoenix::function<expr> const times = expr('*');
-    boost::phoenix::function<expr> const divide = expr('/');
-
-    struct negate_expr
-    {
-        template <typename T1, typename T2 = void>
-        struct result { typedef void type; };
-
-        void operator()(spirit::utree& expr, spirit::utree const& rhs) const
-        {
-            char const op = '-';
-            expr.clear();
-            expr.push_back(spirit::utf8_symbol_range_type(&op, &op+1));
-            expr.push_back(rhs);
-        }
-    };
-    boost::phoenix::function<negate_expr> neg;
 
     ///////////////////////////////////////////////////////////////////////////////
-    //  Our calculator grammar
+    //  The calculator grammar
     ///////////////////////////////////////////////////////////////////////////////
     template <typename Iterator>
-    struct calculator : qi::grammar<Iterator, ascii::space_type, spirit::utree()>
+    struct calculator : qi::grammar<Iterator, ast::program(), ascii::space_type>
     {
         calculator() : calculator::base_type(expression)
         {
-            using qi::uint_;
-            using qi::_val;
-            using qi::_1;
+            qi::double_type double_;
+            qi::char_type char_;
 
             expression =
-                term                            [_val = _1]
-                >> *(   ('+' >> term            [plus(_val, _1)])
-                    |   ('-' >> term            [minus(_val, _1)])
+                term
+                >> *(   (char_('+') >> term)
+                    |   (char_('-') >> term)
                     )
                 ;
 
             term =
-                factor                          [_val = _1]
-                >> *(   ('*' >> factor          [times(_val, _1)])
-                    |   ('/' >> factor          [divide(_val, _1)])
+                factor
+                >> *(   (char_('*') >> factor)
+                    |   (char_('/') >> factor)
                     )
                 ;
 
             factor =
-                uint_                           [_val = _1]
-                |   '(' >> expression           [_val = _1] >> ')'
-                |   ('-' >> factor              [neg(_val, _1)])
-                |   ('+' >> factor              [_val = _1])
+                    double_
+                |   '(' >> expression >> ')'
+                |   (char_('-') >> factor)
+                |   (char_('+') >> factor)
                 ;
-
-            BOOST_SPIRIT_DEBUG_NODE(expression);
-            BOOST_SPIRIT_DEBUG_NODE(term);
-            BOOST_SPIRIT_DEBUG_NODE(factor);
         }
 
-        qi::rule<Iterator, ascii::space_type, spirit::utree()> expression, term, factor;
+        qi::rule<Iterator, ast::program(), ascii::space_type> expression;
+        qi::rule<Iterator, ast::program(), ascii::space_type> term;
+        qi::rule<Iterator, ast::operand(), ascii::space_type> factor;
     };
 }
 
 ///////////////////////////////////////////////////////////////////////////////
 //  Main program
 ///////////////////////////////////////////////////////////////////////////////
-int main()
+int
+main()
 {
     std::cout << "/////////////////////////////////////////////////////////\n\n";
     std::cout << "Expression parser...\n\n";
     std::cout << "/////////////////////////////////////////////////////////\n\n";
     std::cout << "Type an expression...or [q or Q] to quit\n\n";
 
-    using boost::spirit::ascii::space;
-    using boost::spirit::utree;
     typedef std::string::const_iterator iterator_type;
     typedef client::calculator<iterator_type> calculator;
-
-    calculator calc; // Our grammar
+    typedef client::ast::program ast_program;
+    typedef client::ast::printer ast_print;
+    typedef client::ast::eval ast_eval;
 
     std::string str;
     while (std::getline(std::cin, str))
@@ -136,15 +250,22 @@ int main()
         if (str.empty() || str[0] == 'q' || str[0] == 'Q')
             break;
 
+        calculator calc;        // Our grammar
+        ast_program program;    // Our program (AST)
+        ast_print print;        // Prints the program
+        ast_eval eval;          // Evaluates the program
+
         std::string::const_iterator iter = str.begin();
         std::string::const_iterator end = str.end();
-        utree ut;
-        bool r = phrase_parse(iter, end, calc, space, ut);
+        boost::spirit::ascii::space_type space;
+        bool r = phrase_parse(iter, end, calc, space, program);
 
         if (r && iter == end)
         {
             std::cout << "-------------------------\n";
-            std::cout << "Parsing succeeded: " << ut << "\n";
+            std::cout << "Parsing succeeded\n";
+            print(program);
+            std::cout << "\nResult: " << eval(program) << std::endl;
             std::cout << "-------------------------\n";
         }
         else
@@ -152,7 +273,7 @@ int main()
             std::string rest(iter, end);
             std::cout << "-------------------------\n";
             std::cout << "Parsing failed\n";
-            std::cout << "stopped at: \": " << rest << "\"\n";
+            std::cout << "stopped at: \" " << rest << "\"\n";
             std::cout << "-------------------------\n";
         }
     }
