f3286965e194dba9fe070bc17df96046612fb733
Patrick Schulz <pschulz@posteo.de>
2018-11-13 11:47:04 +0100

    Improved options.

--- src/options.hpp
+++ src/options.hpp
@@ -52,10 +52,10 @@ class options
                 ("devices,D" , "print information about the usage of all components")
                 ("nosolve,s" , "don't solve the network")
                 ("report,r"  , "create a report")
-                ("mode,m", value<std::string>()->default_value("ac"), "simulation mode (possible values: ac (default), sp (S-Parameters), tf (transfer function)")
-                ("nodes", value<std::vector<std::string>>()->multitoken()->default_value(std::vector<std::string>{}, ""), "Nodes for transfer function")
+                ("mode,m"    , value<std::string>()->default_value("ac"), "simulation mode (possible values: ac (default), sp (S-Parameters), tf (transfer function)")
+                ("nodes"     , value<std::vector<std::string>>()->multitoken()->default_value(std::vector<std::string>{}, ""), "Nodes for transfer function")
                 ("result"    , "which results should be shown? (show all if none are given)")
-                ("matlab_export"," export results to MatLab-Script, Name must be given")
+                ("matlab"    , value<std::string>(), "export results to MatLab, specify filename")
                 ;
 
             options_description hidden("Hidden options");
