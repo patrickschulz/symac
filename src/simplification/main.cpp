cb9e9237095fae14071f3385856b858e9d4002b4
Patrick Schulz <pschulz@posteo.de>
2018-12-27 16:45:37 +0100

    Merged tsort graph stuff into simplification

--- src/simplification/main.cpp
+++ src/simplification/main.cpp
@@ -5,53 +5,48 @@
 
 #include "../symbol.hpp"
 #include "simplification.hpp"
-
-template<typename T>
-void print_vector(const std::vector<T>& vec)
-{
-    std::cout << "{ ";
-    for(const auto& t : vec)
-    {
-        std::cout << t << ", ";
-    }
-    std::cout << "}\n";
-}
+#include "weightmap.hpp"
 
 int main()
 {
-    GiNaC::symbol R1("R1");
-    GiNaC::symbol R2("R2");
-    GiNaC::symbol C1("C1");
-    GiNaC::symbol C2("C2");
-    GiNaC::symbol gm("gm");
-    GiNaC::symbol rout("rout");
-    GiNaC::symbol gm1("gm1");
-    GiNaC::symbol gm2("gm2");
-    GiNaC::symbol rout1("rout1");
-    GiNaC::symbol rout2("rout2");
-    GiNaC::symbol Cl("Cl");
-    GiNaC::symbol Cs("Cs");
+    GiNaC::symbol R1 = get_symbol("R1");
+    GiNaC::symbol R2 = get_symbol("R2");
+    GiNaC::symbol C1 = get_symbol("C1");
+    GiNaC::symbol C2 = get_symbol("C2");
+    GiNaC::symbol gm = get_symbol("gm");
+    GiNaC::symbol rout = get_symbol("rout");
+    GiNaC::symbol gm1 = get_symbol("gm1");
+    GiNaC::symbol gm2 = get_symbol("gm2");
+    GiNaC::symbol rout1 = get_symbol("rout1");
+    GiNaC::symbol rout2 = get_symbol("rout2");
+    GiNaC::symbol Cl = get_symbol("Cl");
+    GiNaC::symbol Cs = get_symbol("Cs");
+    GiNaC::symbol sym1 = get_symbol("1");
 
     GiNaC::symbol s = get_symbol("s");
 
-    std::map<GiNaC::symbol, int, GiNaC::ex_is_less> weightmap
-    {
-        { R1, 1 },
-        { R2, 0 },
-        { C1, 1 },
-        { C2, 0 },
-        { gm, 1 },
-        { rout, 1 },
-        { gm1, 1 },
-        { rout1, 1 },
-        { gm2, 1 },
-        { rout2, 1 },
-        { Cl, 0 },
-        { Cs, 0 }
+    std::vector<inequality> inequalities {
+        { R1, R2, ">>" },
+        { C1, C2, ">>" },
+        { gm, sym1, ">>" },
+        { rout, sym1, ">>" },
+        { gm1, sym1, ">>" },
+        { rout1, sym1, ">>" },
+        { gm2, sym1, ">>" },
+        { rout2, sym1, ">>" }
     };
 
-    //transfer_function tf(gm1 * rout1 * gm2 * rout2 + gm1 * rout1 / (1 + s * (rout1 * Cl + rout2 * Cl + gm2 * rout2 * rout1 * Cl)));
+    auto weightmap = compute_weightmap(inequalities);
+
+    /*
     GiNaC::ex expr = (gm1 * rout1 * gm2 * rout2 + gm1 * rout1) / (1 + s * rout1 * Cl + s * rout2 * Cl + s * gm2 * rout2 * rout1 * Cl);
+    transfer_function tf(expr);
+    tf.pretty_print(std::cout);
+    transfer_function simple_tf = simplify(tf, weightmap);
+    simple_tf.pretty_print(std::cout);
+    */
+
+    GiNaC::ex expr = (R1 * R2) / (R1 + R2);
     transfer_function tf(expr);
     tf.pretty_print(std::cout);
     transfer_function simple_tf = simplify(tf, weightmap);
--- src/simplification/topological_sort.cpp
+++
@@ -1,1 +0,0 @@
-#include "topological_sort.hpp"
