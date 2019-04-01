a2cb8774fd62464ceffa658e41726fe966739083
Patrick Schulz <pschulz@posteo.de>
2019-04-01 17:36:26 +0200

    started with noise

---
+++ src/noise.hpp
@@ -0,0 +1,11 @@
+#ifndef NOISE_HPP
+#define NOISE_HPP
+
+#include "transfer_function/transfer_function.hpp"
+
+void noise_bandwidth(const transfer_function& tf)
+{
+    std::vector
+}
+
+#endif // NOISE_HPP
